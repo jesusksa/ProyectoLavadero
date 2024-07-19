65ce3d10e587e8ce5ba03b57f795858130c54749
joaquinpralong2002 <115091491+joaquinpralong2002@users.noreply.github.com>
2024-07-18 23:10:02 -0300

    Agregado mapeo a la clase Turno y Empleado.

--- Codigo/lavadero/src/main/java/com/lavadero/model/Empleado.java
+++ Codigo/lavadero/src/main/java/com/lavadero/model/Empleado.java
@@ -1,14 +1,24 @@
 package com.lavadero.model;
+import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.NaturalId;
 
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "empleado")
 public class Empleado {
-    private String nombre;
-    private String apellido;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_empleado", nullable = false)
+    private Long idEmpleado;
+
+    private String nombres;
+    private String apellidos;
+
+    @NaturalId
     private int dni;
 
 }
--- Codigo/lavadero/src/main/java/com/lavadero/model/Turno.java
+++ Codigo/lavadero/src/main/java/com/lavadero/model/Turno.java
@@ -2,6 +2,8 @@ package com.lavadero.model;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+
+import jakarta.persistence.*;
 import lombok.*;
 
 
@@ -9,17 +11,34 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity(name = "turno")
 public class Turno {
-    private int idTurno;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_turno", nullable = false)
+    private Long idTurno;
+
+    @Column(name = "fecha_turno")
     private LocalDate fechaTurno;
+
+    @Column(name = "hora_ingreso")
     private LocalTime horaIngreso;
+
+    @Column(name = "hora_finalizado")
     private LocalTime horaFinalizado;
+
     private int plataforma;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo_servicio")
     private TipoServicio tipoServicio;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "forma_pago")
     private FormaPago formaPago;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "estado_lavado")
     private EstadoLavado estado;
 
-    
-
-    
 }
