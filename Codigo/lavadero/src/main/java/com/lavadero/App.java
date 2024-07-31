55c2e229a06d6935b48e77bfc30003146caa634f
jesusksa <72816948+jesusksa@users.noreply.github.com>
2024-07-31 12:18:35 -0300

    Update, Del ventanas

--- Codigo/lavadero/src/main/java/com/lavadero/App.java
+++ Codigo/lavadero/src/main/java/com/lavadero/App.java
@@ -21,7 +21,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("/views/VentanaIniciarSesion"), 640, 480);
+        scene = new Scene(loadFXML("VentanaIniciarSesion"));
         stage.setScene(scene);
         stage.show();
     }
@@ -31,7 +31,7 @@ public class App extends Application {
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/views/"+fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
