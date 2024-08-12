dcdcdfcb3c541edf2d3070ab9266912e10a902c0
jesusksa <72816948+jesusksa@users.noreply.github.com>
2024-08-12 20:59:10 -0300

    Update Ventana base

--- Codigo/lavadero/src/main/java/com/lavadero/controllers/BaseController.java
+++ Codigo/lavadero/src/main/java/com/lavadero/controllers/BaseController.java
@@ -11,45 +11,17 @@ import java.io.IOException;
 import java.util.Optional;
 
 public class BaseController {
-    public MenuItem mItmGesTurn;
-    public MenuItem mItmRegTurn;
-    public MenuItem mItmGesEmp;
-    public MenuItem mItmAltaEmp;
-    public MenuItem mItmBajaEmp;
-    public MenuItem mItmInfo;
-    public MenuItem mItemEditarCuenta;
-    public MenuItem mItmSalir;
     public Pane panePrincipal;
-
-    public void GestionarTurnos(ActionEvent actionEvent) {
-    }
 
     public void RegistrarTurno(ActionEvent actionEvent) {
     }
 
-    public void GestionarEmpleados(ActionEvent actionEvent) {
+    public void PagTurnos(ActionEvent actionEvent) {
     }
 
-    public void AltaEmpleado(ActionEvent actionEvent) {
+    public void PagEmpleados(ActionEvent actionEvent) {
     }
 
-    public void BajaEmpleado(ActionEvent actionEvent) {
-    }
-
-    public void InformacionCuenta(ActionEvent actionEvent) {
-    }
-
-    public void EditarCuenta(ActionEvent actionEvent) {
-    }
-
-    public void CerrarSesion(ActionEvent actionEvent) throws IOException {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setTitle("Cerrar sesión");
-        alert.setHeaderText("¿Estás seguro de que deseas cerrar sesión?");
-        Optional<ButtonType> resultado = alert.showAndWait();
-
-        if (resultado.get() == ButtonType.OK) {
-            App.setRoot("inicio-sesion");
-        }
+    public void PagCuenta(ActionEvent actionEvent) {
     }
 }
