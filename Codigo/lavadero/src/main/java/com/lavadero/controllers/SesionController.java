162e6996cd3360624a6563566724aeb96c8d6b5b
joaquinpralong2002 <115091491+joaquinpralong2002@users.noreply.github.com>
2025-01-16 18:34:05 -0300

    Agregadas verificaciones para las credenciales de inicio de sesión.

--- Codigo/lavadero/src/main/java/com/lavadero/controllers/SesionController.java
+++ Codigo/lavadero/src/main/java/com/lavadero/controllers/SesionController.java
@@ -5,9 +5,9 @@ import javafx.beans.InvalidationListener;
 import javafx.beans.Observable;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
+import javafx.scene.control.*;
+
+import java.util.regex.Pattern;
 
 public class SesionController {
     @FXML
@@ -18,6 +18,8 @@ public class SesionController {
     private TextField campoContraseniaDescubierta;
     @FXML
     private PasswordField campoContrasenia;
+    @FXML
+    private Button botonAceptar;
 
     @FXML
     private void mostrarContrasenia(ActionEvent actionEvent) {
@@ -37,10 +39,49 @@ public class SesionController {
     @FXML
     public void initialize() {
         campoContraseniaDescubierta.setVisible(false);
+    }
+
+    @FXML
+    private void validarCredenciales(ActionEvent actionEvent) {
+        String nombreUsuario = campoNombre.getText();
+        String contrasenia = campoContrasenia.getText();
+
+        if(campoContraseniaDescubierta.isVisible()){
+            contrasenia = campoContraseniaDescubierta.getText();
+        } else {
+            contrasenia = campoContrasenia.getText();
+        }
+
+        //No se completó alguno de los campos
+        if(nombreUsuario.isEmpty() || contrasenia.isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No se han rellenado todos los campos.");
+            alert.show();
+        }
+
+        //Longitud de nombre de usuario incorrecta
+        else if (nombreUsuario.length() < 5 || nombreUsuario.length() > 30){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "La longitud de nombre de usuario ingresada no es adecuada.");
+            alert.show();
+
+        //Nombre de usuario con caracteres especiales
+        } else if (!nombreUsuario.chars().allMatch(Character::isLetter)){
+            Alert alert = new Alert(Alert.AlertType.ERROR, "No se permiten carácteres especiales en el nombre de usuario.");
+            alert.show();
+
+        //Contraseña con formato incorrecto
+        } else if (!validarContrasenia(contrasenia)) {
+            Alert alert = new Alert(Alert.AlertType.ERROR, "Formato de contraseña incorrecto. La contraseña " +
+                    "debe tener una longitud mínima de 10 carácteres e incluir al menos un número.");
+            alert.show();
+        }
 
 
     }
 
+    private boolean validarContrasenia(String contrasenia){
+        String regex = "^(?=.*\\d).{10,}$";
+        return Pattern.matches(regex, contrasenia);
+    }
 
 
 }
