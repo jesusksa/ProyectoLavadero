0a2c70ca2f30bbf053addab110d36bc66cbf689a
joaquinpralong2002 <115091491+joaquinpralong2002@users.noreply.github.com>
2025-01-21 00:06:08 -0300

    Modificado pasaje de Stage principal entre controladores. (De ahora en adelante utilizar App.getMainStage() ).

--- Codigo/lavadero/src/main/java/com/lavadero/App.java
+++ Codigo/lavadero/src/main/java/com/lavadero/App.java
@@ -50,6 +50,7 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
+
         launch();
         //SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
     }
