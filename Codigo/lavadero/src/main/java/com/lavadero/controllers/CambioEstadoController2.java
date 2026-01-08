f79bacb983ccbbadefcba718c6efdcada13d9827
jesusksa <ksajesusedu@gmail.com>
2026-01-08 10:30:19 -0300

    Create ventana de cambio de estado, y edicion de hora

---
+++ Codigo/lavadero/src/main/java/com/lavadero/controllers/CambioEstadoController2.java
@@ -0,0 +1,20 @@
+package com.lavadero.controllers;
+
+import java.awt.event.ActionEvent;
+
+public class CambioEstadoController2 {
+    public void cambioAEspera(ActionEvent event){
+    }
+
+    public void cambioAProceso(ActionEvent event){
+    }
+
+    public void cambioAFinalizado(ActionEvent event){
+    }
+
+    public void cambioACancelado(ActionEvent event){
+    }
+
+    public void confirmarEdicionTurno(ActionEvent event){
+    }
+}
