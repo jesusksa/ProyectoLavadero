d66db7c06c7ea835d7d02f87b1fb5e1d854d64af
jesusksa <ksajesusedu@gmail.com>
2026-01-09 13:20:00 -0300

    Update ventanas de cambio de estado

--- Codigo/lavadero/src/main/java/com/lavadero/controllers/CambioEstadoController2.java
+++ Codigo/lavadero/src/main/java/com/lavadero/controllers/CambioEstadoController2.java
@@ -1,20 +1,51 @@
 package com.lavadero.controllers;
 
-import java.awt.event.ActionEvent;
+import com.lavadero.App;
+import javafx.event.ActionEvent;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.control.TextArea;
+
+
+import java.io.IOException;
 
 public class CambioEstadoController2 {
-    public void cambioAEspera(ActionEvent event){
+
+    public AnchorPane apnContenidoAjusteEstado;
+    public GridPane gdResponsables;
+    public TextArea txtMotivoCancelacion;
+
+    public void cambioAEspera(ActionEvent actionEvent){
     }
 
-    public void cambioAProceso(ActionEvent event){
+    public void cambioAProceso(ActionEvent actionEvent){
     }
 
-    public void cambioAFinalizado(ActionEvent event){
+    public void cambioAFinalizado(ActionEvent actionEvent) throws IOException {
+        cargarContenidoCambiEstado(App.loadFXML("cambio-estado-finalizado"));
     }
 
-    public void cambioACancelado(ActionEvent event){
+    public void cambioACancelado(ActionEvent actionEvent) throws IOException {
+        cargarContenidoCambiEstado(App.loadFXML("cambio-estado-cancelado"));
     }
 
-    public void confirmarEdicionTurno(ActionEvent event){
+    public void confirmarEdicionTurno(ActionEvent actionEvent){
     }
+
+    public void confirmarFinalizacion(ActionEvent actionEvent){
+    }
+
+    public void confirmarCancelacion(ActionEvent actionEvent){
+    }
+
+    public void cargarContenidoCambiEstado(Parent p){
+        apnContenidoAjusteEstado.getChildren().clear();
+        apnContenidoAjusteEstado.getChildren().setAll(p);
+        AnchorPane.setTopAnchor(p, 0.0);
+        AnchorPane.setBottomAnchor(p, 0.0);
+        AnchorPane.setLeftAnchor(p, 0.0);
+        AnchorPane.setRightAnchor(p, 0.0);
+    }
+
 }
