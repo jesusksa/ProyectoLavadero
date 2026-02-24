8855642f7caba2175e76ea4c5a78b0f3a1df9966
jesusksa <ksajesusedu@gmail.com>
2026-02-24 15:26:15 -0300

    Fix problema de seguridad

--- Codigo/lavadero/src/main/java/com/lavadero/util/ServicioSMS.java
+++ Codigo/lavadero/src/main/java/com/lavadero/util/ServicioSMS.java
@@ -6,9 +6,9 @@ import com.twilio.type.PhoneNumber;
 
 public class ServicioSMS {
 
-    private static final String ACCOUNT_SID = "ACbd938248770fb67c1d984d241a3f986d";
-    private static final String AUTH_TOKEN = "fe8f069ddf1d082a161da6fcab23a631";
-    private static final String FROM_NUMBER = "+14475743417";
+    private static final String ACCOUNT_SID = "TU_ACCOUNT_SID";
+    private static final String AUTH_TOKEN = "TU_AUTH_TOKEN";
+    private static final String FROM_NUMBER = "THE_NUMBER";
 
     public void enviarSMS(String numeroDestino, String mensaje) {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
