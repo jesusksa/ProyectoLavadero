04f7dd50edfed68144116e295b2911bc106f381e
jesusksa <ksajesusedu@gmail.com>
2026-02-24 17:33:58 -0300

    Update ruta dinamica

--- Codigo/lavadero/src/main/java/com/lavadero/util/HibernateUtil.java
+++ Codigo/lavadero/src/main/java/com/lavadero/util/HibernateUtil.java
@@ -3,13 +3,33 @@ package com.lavadero.util;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.io.File;
+
 public class HibernateUtil {
     private static final SessionFactory sessionFactory = buildSessionFactory();
 
     private static SessionFactory buildSessionFactory() {
         try {
-            // Create the SessionFactory from hibernate.cfg.xml
-            return new Configuration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration().configure();
+
+            // 🔹 Ruta dinámica profesional en AppData
+            String userHome = System.getProperty("user.home");
+            String dbFolder = userHome + "\\AppData\\Local\\Lavadero";
+
+            // Crear carpeta si no existe
+            File folder = new File(dbFolder);
+            if (!folder.exists()) {
+                folder.mkdirs();
+            }
+
+            String dbPath = dbFolder + "\\lavadero";
+
+            configuration.setProperty(
+                    "hibernate.connection.url",
+                    "jdbc:h2:" + dbPath
+            );
+
+            return configuration.buildSessionFactory();
         } catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
             System.err.println("Initial SessionFactory creation failed." + ex);
